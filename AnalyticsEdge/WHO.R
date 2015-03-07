752b344f982b565b5ff60c640f0aa1e94f2a7e0e
Gerrit <gerrit.gruben@gmail.com>
2015-03-07 10:37:35 +0100

    Solved part about the WHO data set

---
+++ AnalyticsEdge/WHO.R
@@ -0,0 +1,16 @@
+WHO = csv.load("WHO.csv")
+names(WHO)
+summary(WHO$Over60)
+
+minIndex = which.min(WHO$Over60)
+WHO$Country[minIndex]
+
+maxIndex = which.max(WHO$LiteracyRate)
+WHO$Country[maxIndex]
+
+outliers = subset(WHO, GNI > 100000 & FertilityRate > 2.5)
+nrows(outliers)
+outliers[c("Country", "GNI", "FertilityRate")]
+hist(outliers)
+
+tapply(WHO$ChildMortality, WHO$Region, mean)
