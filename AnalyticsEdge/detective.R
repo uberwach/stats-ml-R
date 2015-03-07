8f724400346bda57cfa3403f2252e8bd875446bb
Gerrit <gerrit.gruben@gmail.com>
2015-03-07 20:04:19 +0100

    Finished first exercise.

---
+++ AnalyticsEdge/detective.R
@@ -0,0 +1,36 @@
+# An Analytical Detective
+mvt = read.csv("mvtWeek1.csv")
+
+# Problem 1
+nrow(mvt)
+names(mvt)
+max(mvt$ID)
+min(mvt$Beat)
+table(mvt$Arrest)
+nrow(subset(mvt, LocationDescription == 'ALLEY'))
+
+# Problem 2
+head(mvt$Date)
+DateConvert = as.Date(strptime(mvt$Date, "%m/%d/%y %H:%M"))
+median(DateConvert)
+
+mvt$Month = months(DateConvert)
+mvt$Weekday = weekdays(DateConvert)
+mvt$Date = DateConvert
+table(mvt$Month)
+table(mvt$Weekday)
+table(subset(mvt, Arrest)$Month)
+
+# Problem 3
+hist(mvt$Date, breaks = 100)
+boxplot(Date ~ Arrest, data = mvt)
+table(subset(mvt, Year == 2001)$Arrest)
+table(subset(mvt, Year == 2007)$Arrest)
+table(subset(mvt, Year == 2012)$Arrest)
+
+# Problem 4
+sort(table(mvt$LocationDescription))
+Top5 = subset(mvt, is.element(LocationDescription, c("STREET", "ALLEY","PARKING LOT/GARAGE(NON.RESID.)", "GAS STATION", "DRIVEWAY - RESIDENTIAL")))
+nrow(Top5)
+Top5$LocationDescription = factor(Top5$LocationDescription)
+tapply(as.numeric(Top$Arrest),Top5$LocationDescription, avg)
