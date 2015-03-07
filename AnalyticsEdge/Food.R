abc0080ca2a34582b0f7faab5e2aca7948a4c2af
Gerrit <gerrit.gruben@gmail.com>
2015-03-07 16:23:17 +0100

    ups didn't save file

--- AnalyticsEdge/Food.R
+++ AnalyticsEdge/Food.R
@@ -24,3 +24,6 @@ sd(USDA$Sodium, na.rm = TRUE)
 
 # Adding variables
 USDA$HighSodium = as.numeric(USDA$Sodium > mean(USDA$Sodium, na.rm=TRUE))
+USDA$HighProtein = as.numeric(USDA$Protein > mean(USDA$Protein, na.rm=TRUE))
+USDA$HighFat = as.numeric(USDA$TotalFat > mean(USDA$TotalFat, na.rm=TRUE))
+USDA$HighCarbs = as.numeric(USDA$Carbohydrate > mean(USDA$Carbohydrate, na.rm=TRUE))
